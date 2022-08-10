98548eec8e2199b1c0bc3c6f35395286eb1f52f2
Alex <allie2005nicholas@gmail.com>
2022-08-10 10:46:04 -0400

    data types and variables

---
+++ app.js
@@ -0,0 +1,20 @@
+let bool = '1' == 1
+
+console.log(bool)
+
+// _----------------------
+// let FullName = 'alex'
+
+// const inRaing = true
+// let temperature = 30
+// let planet = 'earth'
+
+// temperature = temperature + 2
+
+// console.log(temperature)
+// -----------------------------------
+// let celsius = 10;
+
+// let fahrenheit = celsius * 1.8 + 32
+
+// console.log(fahrenheit)
