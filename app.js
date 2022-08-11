e595d81b159ae788f43e2deda5af694e9d30d3d5
Alex <allie2005nicholas@gmail.com>
2022-08-11 15:13:03 -0400

    went over truthy falsy statements

--- app.js
+++ app.js
@@ -1,6 +1,81 @@
-let bool = '1' == 1
+// ternary operators
 
-console.log(bool)
+let subscribed = true
+let loggedIn = true
+
+let str = subscribed && loggedIn ? 'show the video' : 'hide the video'
+console.log(str);
+
+
+// ^
+// let hot = true
+
+// hot ? console.log('weather is hot outside') : console.log('wather is cold')
+
+
+
+
+
+
+// truthy falsy stuff
+// let val = "ccunt";
+
+// if (val) {
+//     console.log("truthy value", !!val)
+// }
+// else {
+//     console.log("falsy value", !!val)
+// }
+
+
+
+
+
+
+// let cash = 50
+// let price = 40
+// let isStoreOpen = false
+
+
+//  if you have enough cash to get the item from the store, and the store is open, then you print the receipt
+// if (cash >= price && !isStoreOpen) {
+//     console.log('print the receipt')
+// }
+
+// if (cash >= price) {
+//     console.log('you have enough cash to buy the product')
+// }
+
+
+
+
+
+// CONDITIONALS
+
+// let cash = 30
+// let price = 40
+// let difference = cash - price
+
+// if (cash > price) {
+//     console.log(`you paid extra - here's ${difference} dollars change`)
+// }
+// else if (cash === price) {
+//     console.log("you paid the exact ammount, have a nice day!")
+// }
+// else {
+//     console.log (`not enough money - you still owe ${difference * -1} dollares`)
+// }
+
+
+
+
+
+
+
+// data types and variables
+// let bool = '1' == 1
+
+// console.log(bool)
 
 // _----------------------
 // let FullName = 'alex'
