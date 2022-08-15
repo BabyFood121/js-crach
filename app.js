9fa5a025529aee4dbcd2e472d9028e9553e56084
Alex <allie2005nicholas@gmail.com>
2022-08-15 16:10:47 -0400

    finished object section, this shi make no sense but i'll undertand it soon

--- app.js
+++ app.js
@@ -1,5 +1,75 @@
-// --- ARRAYS ---
+// --------DOM--------
 
+
+
+
+
+
+// -----OBJECTS-----
+
+// let user = [
+//   {
+//     username: "Alex",
+//     email: "allie2005nicholas@gmail.com",
+//     password: "test123",
+//     userSubscriptionStatus: "VIP+",
+//     discordId: "Kenpachi",
+//     lessonCompleted: [0, 1],
+//   },
+//   {
+//     username: "mitri",
+//     email: "mitri@gmail.com",
+//     password: "mitri123",
+//     userSubscriptionStatus: "VIP+",
+//     discordId: "mitri",
+//     lessonCompleted: [0, 1, 3, 4],
+//   },
+//   {
+//     username: "zen",
+//     email: "zen@gmail.com",
+//     password: "zen123",
+//     userSubscriptionStatus: "VIP+",
+//     discordId: "zen#001",
+//     lessonCompleted: [0, 1, 3, 4],
+//   },
+// ];
+
+// function login(email, password) {
+//   for (let i = 0; i < user.length; ++i) {
+//     if (user[i].email === email) {
+//       console.log(user[i]);
+//       if (user[i].password === password) {
+//         console.log("log the user in - the details are correct");
+//       } else {
+//         console.log("password is incorrect - try again");
+//       }
+//       return;
+//     }
+//   }
+//   console.log("could not find an email that matches");
+// }
+
+// login("allie2005nicholas@gmail.com", "test123");
+
+// // object practice
+
+// function register(user) {
+//     user.push(users);
+// }
+
+// register({
+//     username: "zen", 
+//     email: "zen@gmail.com", 
+//     password: "zen123", 
+//     userSubscriptionStatus: "VIP+", 
+//     discordId: "zen#001", 
+//     lessonCompleted: [0, 1, 3, 4]
+//     });
+
+//     console.log(user);
+
+
+// --- ARRAYS ---
 
 // let dollars = [1, 5, 10, 3]
 
@@ -16,20 +86,11 @@
 // // console log cents
 // console.log(cents)
 
-
-
-
-
-
-
 // let dollar = [1, 5, 10, 3]
 
 // let cents = dollar.map(element => element * 100)
 
 // console.log(cents)
-
-
-
 
 // let dollar = [1, 5, 10, 3]
 
@@ -39,17 +100,11 @@
 
 // console.log(cents)
 
-
-
-
-
 // let arr = [1, 5, 10, 3]
 
 // let newArray = arr.map(element => 'dog')
 
 // console.log(newArray)
-
-
 
 // let arr = [1, 4, 9, 16]
 
@@ -59,12 +114,6 @@
 // })
 
 // console.log(newArray)
-
-
-
-
-
-
 
 // let grades = ['A+', 'A', 'FAIL']
 
@@ -78,24 +127,17 @@
 
 // console.log(goodGrades);
 
-
-
-
-
 // let grades = ['A+', 'A', 'FAIL']
 
 // let goodGrades = grades.filter(element => element !== 'FAIL')
 
 // console.log(goodGrades)
 
-
-
-
 // let arr =[20, 30, 40, 50, 100]
 
 // // [20, 30, 40,]
 // let newArr = arr.filter(element => {
-//     console.log(element) 
+//     console.log(element)
 //     if (element < 50) {
 //         return true;
 //     }
@@ -106,20 +148,13 @@
 
 // console.log(newArr)
 
-
-
-
-// -- FUNCTIONS--- 
+// -- FUNCTIONS---
 
 // const convertCelciusToFahrenehit = (celcius) => {
 //     return celcius * 1.8 + 32
 // }
 
 // console.log(convertCelciusToFahrenehit(0));
-
-
-
-
 
 // function convertCelciusToFahrenehit(celcius) {
 //     return celcius * 1.8 + 32
@@ -128,34 +163,20 @@
 // console.log(convertCelciusToFahrenehit(5));
 // console.log(convertCelciusToFahrenehit(18));
 
-
-
-
 // function sumOfTwoNumbers(num1, num2) {
 //     return num1 + num2
 // }
 
 // console.log(sumOfTwoNumbers(10, 10));
 
-
-
-
-
 // function definition
 // function welcomPersonToFES (FirstName, LastName) {
 // console.log(`welcome to FES, ${FirstName} ${LastName}`)
 // }
-// 
+//
 // call the function
 // welcomPersonToFES('Alex', 'Nicholas');
 // welcomPersonToFES('joe', 'shmoe');
-
-
-
-
-
-
-
 
 // LOOPS
 
@@ -174,16 +195,11 @@
 //     }
 // }
 
-
 // const str = "Frontend Simplified"
 
 // for (let i = 0; i < str.length; ++i){
 //     console.log(str[i])
 // }
-
-
-
-
 
 // DRY = Don't repeat youself
 
@@ -194,41 +210,27 @@
 //     count = count + 1
 // }
 
-
 // for (let i = 0; i < 100; i++) {
 //     console.log(i + 1);
 // }
 
-
 //    'i' less than how many times we wan tto loop
-//                   ^                  
+//                   ^
 //for (let i = 0; i < 100; i++) {
- //        ^                ^
+//        ^                ^
 //    set 'i' to 0     increment 'i' by 1
 //     console.log(i)
 
-
 // }
-
-
-
-
-
-
-
-
-
 
 // ternary operators
 
 // let cash = 40
 // let price = 40
-// let isStoreOpen = true 
+// let isStoreOpen = true
 
 // let str = cash >= price && isStoreOpen ? 'give receipt' : 'dont give receipt'
 // console.log(str);
-
-
 
 // ^
 // let subscribed = true
@@ -237,16 +239,10 @@
 // let str = subscribed && loggedIn ? 'show the video' : 'hide the video'
 // console.log(str);
 
-
 // ^
 // let hot = true
 
 // hot ? console.log('weather is hot outside') : console.log('wather is cold')
-
-
-
-
-
 
 // truthy falsy stuff
 // let val = "ccunt";
@@ -258,15 +254,9 @@
 //     console.log("falsy value", !!val)
 // }
 
-
-
-
-
-
 // let cash = 50
 // let price = 40
 // let isStoreOpen = false
-
 
 //  if you have enough cash to get the item from the store, and the store is open, then you print the receipt
 // if (cash >= price && !isStoreOpen) {
@@ -277,12 +267,8 @@
 //     console.log('you have enough cash to buy the product')
 // }
 
-
-
-
-
 // CONDITIONALS
-// 
+//
 
 // let cash = 30
 // let price = 40
@@ -297,12 +283,6 @@
 // else {
 //     console.log (`not enough money - you still owe ${difference * -1} dollares`)
 // }
-
-
-
-
-
-
 
 // data types and variables
 // let bool = '1' == 1
